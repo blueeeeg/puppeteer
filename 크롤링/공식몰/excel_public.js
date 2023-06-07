802823e65eda220cf13a3997126583fa9747af1a
HC-Gwon <45168134+HC-Gwon@users.noreply.github.com>
2023-06-07 12:28:50 +0900

    Update excel_public.js

--- 크롤링/공식몰/excel_public.js
+++ 크롤링/공식몰/excel_public.js
@@ -5,7 +5,7 @@ const valueChecker = (value) => {
 
 // 엑셀 파일 생성
 const makeFile = async (workbook, name) => {
-  await workbook.xlsx.writeFile(`./공식몰/${name}.xlsx`);
+  await workbook.xlsx.writeFile(`./공식몰/리뷰/${name}.xlsx`);
 };
 
 const fillData = (worksheet, reviewData) => {
