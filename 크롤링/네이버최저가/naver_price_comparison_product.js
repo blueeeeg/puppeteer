2025ef9b3ccca6e355633a24b7646a65d177e0a4
HC-Gwon <gurckddlz@naver.com>
2023-07-02 19:20:19 +0900

    네이버 최저가 끌어오는 url 수정 및 판매 중지된 상품 안받아 오도록 수정

--- 크롤링/네이버최저가/naver_price_comparison_product.js
+++ 크롤링/네이버최저가/naver_price_comparison_product.js
@@ -12,9 +12,11 @@ const wait = async (sec) => {
 const getInfo = async (page, product_count) => {
   let product_name = "";
   let product_url = "";
+  let product_price = "";
 
   try {
     const selector_product = `#content > div.style_content__xWg5l > div.basicList_list_basis__uNBZx > div > div:nth-child(${product_count}) > div > div.product_inner__gr8QR > div.product_info_area__xxCTi > div.product_title__Mmw2K > a`;
+    const selector_price = `#content > div.style_content__xWg5l > div.basicList_list_basis__uNBZx > div > div:nth-child(${product_count}) > div > div > div.product_info_area__xxCTi > div.product_price_area__eTg7I > strong > span > span.price_num__S2p_v`;
 
     await page.waitForSelector(selector_product, { timeout: 1000 });
 
@@ -23,12 +25,18 @@ const getInfo = async (page, product_count) => {
       (element) => element.textContent
     );
     product_url = await page.$eval(selector_product, (element) => element.href);
+    product_price = await page.$eval(
+      selector_price,
+      (element) => element.textContent
+    );
   } catch (err1) {
     console.log("err1 : ", err1);
     try {
       const selector_product = `#content > div.style_content__xWg5l > div.basicList_list_basis__uNBZx > div > div:nth-child(${product_count}) > div > div > div.product_info_area__xxCTi > div.product_title__Mmw2K > a`;
+      const selector_price = `#content > div.style_content__xWg5l > div.basicList_list_basis__uNBZx > div > div:nth-child(${product_count}) > div > div > div.product_info_area__xxCTi > div.product_price_area__eTg7I > strong > span > span.price_num__S2p_v`;
 
       await page.waitForSelector(selector_product, { timeout: 1000 });
+      await page.waitForSelector(selector_price, { timeout: 1000 });
 
       product_name = await page.$eval(
         selector_product,
@@ -38,12 +46,16 @@ const getInfo = async (page, product_count) => {
         selector_product,
         (element) => element.href
       );
+      product_price = await page.$eval(
+        selector_price,
+        (element) => element.textContent
+      );
     } catch (err2) {
       console.log("err2 : ", err2);
     }
   }
 
-  return { name: product_name, url: product_url };
+  return { name: product_name, url: product_url, price: product_price };
 };
 
 const NaverPriceComparisonProductCrawling = async () => {
@@ -57,7 +69,7 @@ const NaverPriceComparisonProductCrawling = async () => {
 
     // 해당 url로 이동
     const url =
-      "https://search.shopping.naver.com/search/all?frm=NVSHMDL&origQuery=%EB%8D%94%EB%9E%A9%EB%B0%94%EC%9D%B4%EB%B8%94%EB%9E%91%EB%91%90&pagingIndex=1&pagingSize=40&productSet=model&query=%EB%8D%94%EB%9E%A9%EB%B0%94%EC%9D%B4%EB%B8%94%EB%9E%91%EB%91%90&sort=price_asc&timestamp=&viewType=list";
+      "https://search.shopping.naver.com/search/all?frm=NVSHMDL&origQuery=%EB%8D%94%EB%9E%A9%EB%B0%94%EC%9D%B4%EB%B8%94%EB%9E%91%EB%91%90&pagingIndex=1&pagingSize=40&productSet=model&query=%EB%8D%94%EB%9E%A9%EB%B0%94%EC%9D%B4%EB%B8%94%EB%9E%91%EB%91%90&sort=rel&timestamp=&viewType=list";
     await page.goto(url);
     await page.evaluate(() => {
       window.scrollTo(0, 300);
@@ -68,9 +80,9 @@ const NaverPriceComparisonProductCrawling = async () => {
     let product_list = [];
 
     while (true) {
-      const { name, url } = await getInfo(page, product_count);
+      const { name, url, price } = await getInfo(page, product_count);
 
-      if (!name) break;
+      if (!name || !price) break;
 
       wait(1);
       await page.evaluate(() => {
